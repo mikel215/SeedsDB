d930c8722989204ec9215ca5a08eca370b612f2b
mikel <mikelmayorga08@gmail.com>
2019-04-12 21:22:06 -0700

    Initial commit

---
+++ functions/get_login.php
@@ -0,0 +1,13 @@
+<?php
+
+function make_login()
+{
+    ?>
+    <form method="post"
+          action="<?= htmlentities($_SERVER['PHP_SELF'],
+                        ENT_QUOTES) ?>">
+
+    </form>
+<?php
+}
+?>
