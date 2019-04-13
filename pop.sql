4c780007bc4f5a3592b68b5904bbab534cdf129d
Emma Wilcox <erw35@humboldt.edu>
2019-04-13 13:08:06 -0700

    no message

--- pop.sql
+++ pop.sql
@@ -2,3 +2,40 @@ delete from Animal_information;
 delete from Individual;
 delete from cat_info;
 delete from cat_owner;
+
+-- populating the Individual table
+insert into Individual
+values
+(111, 'Johnny', 'Quest', TRUE);
+
+insert into Individual
+values
+(222, 'Gayle', 'Belcher', FALSE);
+
+insert into Individual
+values
+(333, 'Mark', 'Twain', FALSE);
+
+insert into Individual
+values
+(444, 'Kurt', 'Cobain', TRUE);
+
+insert into Individual
+values
+(555, 'Tory', 'Kay', TRUE);
+
+insert into Individual
+values
+(666, 'Yoko', 'Ono', TRUE);
+
+insert into Individual
+values
+(777, 'John', 'Lennon', FALSE);
+
+insert into Individual
+values
+(888, 'Joseph', 'Joestar', TRUE);
+
+insert into Individual
+values
+(999, 'Bob', 'Ross', TRUE);
