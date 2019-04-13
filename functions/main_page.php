0bf94bd7632f6d78df43fbcc6cc34cf8a7b0a9ba
Emma Wilcox <erw35@humboldt.edu>
2019-04-13 15:46:23 -0700

    no message

--- functions/main_page.php
+++ functions/main_page.php
@@ -2,7 +2,18 @@
 function make_main_page()
 {
     ?>
-    <div> 
+    <div id="add_cat">
+    </div>
+
+    <div id="add_owner">
+
+    </div>
+
+    <div id="stats">
+    </div>
+
+    <div id="stats">
+    </div>
             <input type="submit" name="animal_form_submit"/>
         </fieldset>
     </form>
