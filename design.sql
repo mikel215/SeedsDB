9fb76756cf8ffe2f0760cff5f606f99bd668ccc2
dpimentelfelix <42754261+dpimentelfelix@users.noreply.github.com>
2019-04-13 14:36:40 -0700

    Add files via upload

--- design.sql
+++ design.sql
@@ -28,23 +28,23 @@ create table medical_info(
 );
 
 create table cat_info
-{
-  cat_id integer;
-  name varchar2(20);
-  DOB date;
-  intake_date date;
-  adopt_date date;
-  breed varchar2(20);
-  age integer;
-  sex char(1);
-  adopter_id integer;
-  medical_id integer;
-}
+(
+  cat_id integer,
+  name varchar2(20),
+  DOB date,
+  intake_date date,
+  adopt_date date,
+  age integer,
+  sex char(1),
+  adopter_id integer,
+  medical_id integer,
+  foreign key(adopter_id) references individual(individual_id)
+);
 
 create table cat_owner
-{
-  individual_id integer;
-  cat_id integer;
-  foreign key(individual_id) references individual;
-  foreign key(cat_id) references cat_info;
-}
+(
+  cat_id integer,
+  individual_id integer,
+  foreign key(cat_id) references cat_info,
+  foreign key(individual_id) references individual
+);
